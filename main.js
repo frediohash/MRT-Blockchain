e34e17fa397ea7957c36cb44106149ea500d0834
fredio.dimyati <fredio.dimyati@dwidasa.com>
2021-07-12 23:20:23 +0700

    Update

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 const SHA256 = require('crypto-js/sha256');
+
+class Transaction{
+    constructor(fromAddress, toAddress, amount){
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.amount = amount;
+    }
+}
+
 class Block{
     constructor(timestamp, transactions, previousHash=''){
         this.timestamp = timestamp;
@@ -23,17 +32,42 @@ class Blockchain{
     constructor(){
         this.chain = [this.createGenesisBlock()];
         this.difficulty = 2;
+        this.pendingTransactions = [];
+        this.miningReward = 100;
     }
     createGenesisBlock(){
-        return new Block(0, "01/06/2021", "Genesis Block", "0");
+        return new Block("01/06/2021", "Genesis Block", "0");
     }
     getLatestBlock(){
         return this.chain[this.chain.length-1];
     }
-    addBlock(newBlock){
-        newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.mineBlock(this.difficulty);
-        this.chain.push(newBlock);
+    minePendingTransaction(miningRewardAddress){
+        let block = new Block(Date.now(), this.pendingTransactions, this.previousHash);
+        block.mineBlock(this.difficulty);
+
+        console.log("Block Successfully Mined");
+        this.chain.push(block);
+
+        this.pendingTransactions = [
+            new Transaction(null, miningRewardAddress, this.miningReward)
+        ];
+    }
+    createTransaction(transaction){
+        this.pendingTransactions.push(transaction);
+    }
+    getBalanceOfAddress(address){
+        let balance = 0;
+        for(const block of this.chain){
+            for(const trans of block.transactions){
+                if(trans.fromAddress === address){
+                    balance -= trans.amount;
+                }
+                if(trans.toAddress === address){
+                    balance += trans.amount;
+                }
+            }
+        }
+        return balance;
     }
     isChainValid(){
         for(let i=1; i<this.chain.length; i++){
